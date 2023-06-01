61400770cbe79871916d16abec16949f6c38ffde
DaniDhax <DaniDhax@users.noreply.github.com>
2023-06-01 10:48:06 +0200

    feat: Añadido un párrafo por cada fact de la api de los gatos

--- app.js
+++ app.js
@@ -1,46 +1,25 @@
-
 const API_GATOS = "https://catfact.ninja/fact"
-const API_WEATHER = "http://api.weatherstack.com/current?access_key=64a0326c9d5e0681a415d3ebc07c5164&query=Madrid"
+const API_WEATHER =
+  "http://api.weatherstack.com/current?access_key=64a0326c9d5e0681a415d3ebc07c5164&query=Madrid"
 
-const container = document.querySelector(".container");
+const contenedor = document.querySelector(".container")
+const btnTraerDatos = document.getElementById("btnTraerDatos")
 
-const traerDatosAPI1 = () => {
-  fetch(
-    API_GATOS
-  )
+const handleClick = () => {
+  traerData();
+};
+
+btnTraerDatos.addEventListener("click", handleClick)
+
+const traerData = () =>
+  fetch(API_GATOS)
     .then((response) => {
-      if (!response.ok) {
-        throw new Error("Error en la solicitud: " + response.status);
-      }
-      return response.json();
+      return response.json()
     })
     .then((data) => {
-      console.log(data);
-      container.innerHTML = data.fact;
+      console.log(data)
+      contenedor.appendChild(document.createElement("p")).textContent = data.fact
     })
     .catch((error) => {
-      console.log("Se ha producido un error:", error);
+      console.log(error)
     });
-};
-
-const handlerClickBtnTraerDatos = () => {
-  console.log("Click")
-  traerDatosAPI1()
-};
-
-
-const btnTrerDatos = document.getElementById("btnTraerDatos");
-btnTrerDatos.addEventListener("click", handlerClickBtnTraerDatos);
-
-
-// fetch ('URL')
-//     .then (response => {
-//         // Hacer cosas
-//         return response.json()
-//     })
-//     .then (data => {
-//         // Hacer cosas
-//     })
-//     .catch (error => {
-//         // Hacer cosas con el error
-//     })
